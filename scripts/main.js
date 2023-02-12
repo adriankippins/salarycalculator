f5002e24413de940987ebd57caa03a94a6c753fa
Roberto Kippins <robertokippins@iotgy.net>
2023-02-12 16:19:38 -0400

    Modification the JS functions.

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,21 @@ function salaryCalc() {
 let hourlyRate = parseFloat( document.getElementById("hr").value || 0 ); //get Hourly rate from user or use 0 if text box is empty.
 let hoursWorked = parseFloat( document.getElementById("hw").value || 0); //get hours worked form user r use 0 if text box is empty.
 
-let grossPay = hourlyRate * hoursWorked; //Gross Pay Calculation.
+let grossPay = Math.round(hourlyRate * hoursWorked); //Gross Pay Calculation.
 
-let nis = NIS(grossPay); //Call NIS Function to calculate insurance.
-let taxes = PAYE(grossPay) //Call PAYE function to calculate taxes.
+let deductions = PAYE(grossPay).map(Math.round) //Call PAYE function and map the returned array to the Math.round() function the round the returned values.
+//let deductionsRounded = deductions.map(Math.round)
+let taxes = deductions[0]; //Taxes is in index 0 of the returned array.
+let nis = deductions[1];  //Insurance is in index 1 of the returned array.
 let totalDeductions = nis + taxes //Total Desuctions = Insurance plus taxes.
 let netPay = grossPay - totalDeductions //Net Salary calculation
 
 //Push rounded values to corrensponding HTML boxes.
-document.getElementById("gp").value = Math.round(grossPay);
-document.getElementById("nis").value = Math.round(nis);
-document.getElementById("tax").value = Math.round(taxes);
-document.getElementById("td").value = Math.round(totalDeductions);
-document.getElementById("net").value = Math.round(netPay);
+document.getElementById("gp").value = grossPay;
+document.getElementById("nis").value = nis;
+document.getElementById("tax").value = taxes;
+document.getElementById("td").value = totalDeductions;
+document.getElementById("net").value = netPay;
 
 }
 
@@ -31,57 +33,57 @@ function NIS(x) {
         y = 280000 * 0.056;
     }
 
-    return y
+    return y;
 }
 
 //Taxes Function
 function PAYE(z) {
-    let nontax
-    let taxable
-    let tax28
-    let tax40
-    let totalTax = 0 //This one needed to have a value of 0 in case all the other boxes were blank.
-    let nis = NIS(z)
+    let nontax;
+    let taxable;
+    let tax28;
+    let tax40;
+    let totalTax = 0; //This one needed to have a value of 0 in case all the other boxes were blank.
+    let nis = NIS(z);
     
     if (z > 75000 && z <= 225000) {
-        nontax = 75000
-        taxable = z - nis - nontax
-        totalTax = taxable * 0.28
-        return totalTax
+        nontax = 75000;
+        taxable = z - nis - nontax;
+        totalTax = taxable * 0.28;
+        return [totalTax, nis];
 
     } else if (z > 225000) {
-        nontax = z / 3
+        nontax = z / 3;
         
         if ((z - nontax + nis) < 0) {
-            taxable = 0
+            taxable = 0;
          
         } else {
-            taxable = z - nontax - nis
+            taxable = z - nontax - nis;
 
         }
         
         
         if (taxable <= 150000) {
-            tax28 = taxable * 0.28
+            tax28 = taxable * 0.28;
 
         } else {
-            tax28 = 150000 * 0.28
+            tax28 = 150000 * 0.28;
 
         }
             
         if (taxable - 150000 > 0) {
-            tax40 = (taxable - 150000) * 0.40
+            tax40 = (taxable - 150000) * 0.40;
 
         } else {
-            tax40 = tax40
+            tax40 = tax40;
         }
 
-        totalTax = tax28 + tax40
+        totalTax = tax28 + tax40;
         
-        return totalTax
+        return [totalTax, nis];
 
     } else {
     
-        return totalTax //Line 43 returned here.
+        return [totalTax, nis]; //taxes and insurance returned as an array.
     }
 }
